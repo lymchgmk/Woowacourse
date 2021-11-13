ff6edb296fdb94d3b7054e4e288a5ab6e94e68d9
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-11-13 21:15:02 +0900

    Update BaseballGame.java

--- 0_Self Precourse/java-baseball-precourse-master/My codes/baseball game/src/BaseballGame.java
+++ 0_Self Precourse/java-baseball-precourse-master/My codes/baseball game/src/BaseballGame.java
@@ -2,9 +2,13 @@ import Constant;
 
 
 public class BaseballGame {
-    public static int[] target = new int[3];
+    public int[] answer = new int[3];
     public static int strike = 0;
     public static int ball = 0;
+
+    public BaseballGame(int[] threeRandomNumbers) {
+        this.answer = threeRandomNumbers;
+    }
 
     public void generateRandomNumber() {
         // 어디에 언제 저장할지 생각해볼것
@@ -16,13 +20,41 @@ public class BaseballGame {
         
     }
 
-    public void judgeBallOrStrike() {
+    public void judgeBallOrStrike(int[] args) {
         // 사용자의 답과 target을 비교하는 메소드
     }
 
-    public void continueGame() {
+    public void continueGame(int isContinue) {
         // 게임을 계속 할지 판단하는 메소드
             // 단 judgeBallOrStrike의 리턴값에 의존
+    }
+
+    public static void main(String args) {
+        int[] threeRandonNumbers = generateRandomNumber();
+        int[] strikeOrBall = int[3];
+        BaseballGame baseballgame = new BaseballGame(threeRandomNumbers);
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i=0; i<3; i++) {
+            if judgeBallOrStrike(answer, scanner) == "Ball" {
+                balls += 1;
+            }
+            else if judgeBallOrStrike(answer, scanner) == "Strike" {
+                strikes += 1;
+            }
+        }
+
+        if strikes == 3 {
+            return Constant
+        }
+        else {
+            coninue
+        }
+
+
+
+
+
     }
 
 }
