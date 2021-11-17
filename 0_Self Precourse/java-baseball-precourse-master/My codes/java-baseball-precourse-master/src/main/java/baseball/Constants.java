f0c721d2edcfb4d445dba2d8cd5897253abada73
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-11-17 23:55:03 +0900

    Update Constants.java

--- 0_Self Precourse/java-baseball-precourse-master/My codes/java-baseball-precourse-master/src/main/java/baseball/Constants.java
+++ 0_Self Precourse/java-baseball-precourse-master/My codes/java-baseball-precourse-master/src/main/java/baseball/Constants.java
@@ -2,8 +2,8 @@ package baseball;
 
 public class Constants {
 	public static final int ANSWER_LENGTH = 3;
-	public static final int MAX_DIGIT_VALUE = 1;
-	public static final int MIN_DIGIT_VALUE = 9;
+	public static final int MIN_DIGIT_VALUE = 1;
+	public static final int MAX_DIGIT_VALUE = 9;
 	
 	public static final int[] DEFAULT_ANSWER = {0, 0, 0};
 	
@@ -11,4 +11,8 @@ public class Constants {
 	public static final String INPUT_NUM_MSG = "숫자를 입력해주세요 : ";	
 	public static final String GAME_END_MSG = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 	public static final String GAME_CONTINUE_MSG = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+	public static final String GAME_EXIT_MSG = "게임을 종료하겠습니다. 감사합니다.";
+	
+	public static final int CONTINUE_GAME = 1;
+	public static final int STOP_GAME = 2;
 }
