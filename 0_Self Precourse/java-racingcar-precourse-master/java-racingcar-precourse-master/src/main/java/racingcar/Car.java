0dafbe779f31dbc44b55585a0d5954ce3dc1be0b
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-11-19 21:53:18 +0900

    레이싱게임 시작 및 계속

--- 0_Self Precourse/java-racingcar-precourse-master/java-racingcar-precourse-master/src/main/java/racingcar/Car.java
+++ 0_Self Precourse/java-racingcar-precourse-master/java-racingcar-precourse-master/src/main/java/racingcar/Car.java
@@ -1,12 +1,24 @@
 package racingcar;
 
+import utils.RandomUtils;
+
 public class Car {
     private final String name;
     private int position = 0;
+    private boolean canRun = true;
 
     public Car(String name) {
         this.name = name;
     }
 
     // 추가 기능 구현
+    protected void go(Car car) {
+    	int zeroToNine = RandomUtils.nextInt(0, 9);
+    	if (zeroToNine >= 4 && canRun) {
+    		position++;
+    	}
+    	else {
+    		canRun = false;
+    	}
+    }
 }
---
+++ 0_Self Precourse/java-racingcar-precourse-master/java-racingcar-precourse-master/src/main/java/racingcar/Constants.java
@@ -0,0 +1,7 @@
+package racingcar;
+
+public class Constants {
+	public static final String GAME_START_MSG = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	
+	public static final String GAME_WINNERS = "최종 우승자: ";
+}
