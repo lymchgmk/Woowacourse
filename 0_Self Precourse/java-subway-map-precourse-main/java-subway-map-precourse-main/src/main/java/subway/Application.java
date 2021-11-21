887a6be288d933f3cda2af326fb43daa4ee68c6e
lymchgmk <lymchgmk@gmail.com>
2021-11-21 21:42:10 +0900

    Update Application.java

--- 0_Self Precourse/java-subway-map-precourse-main/java-subway-map-precourse-main/src/main/java/subway/Application.java
+++ 0_Self Precourse/java-subway-map-precourse-main/java-subway-map-precourse-main/src/main/java/subway/Application.java
@@ -1,10 +1,16 @@
 package subway;
 
 import java.util.Scanner;
+import subway.domain.Page;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO: 프로그램 구현
+        Page mainPage = new Page();
+        Page stationManagePage = new Page();
+        Page lineManagePage = new Page();
+        Page sectionManagePage = new Page();
+        Page mapManagePage = new Page();
     }
 }
